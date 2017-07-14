42e0cb6fc3ad7bc2045dc0307ad117637ba16b61
Arvinky <otw0909@gmail.com>
2017-07-14 18:52:45 +0800

    文档更新

--- TelinkBlueDemo/CryptoAction.m
+++ TelinkBlueDemo/CryptoAction.m
@@ -23,24 +23,18 @@
     return YES;
 }
 
-+(BOOL)encryptPair:(NSString *)uName Pas:(NSString *)uPas Prand:(uint8_t *)prand PResult:(uint8_t *)presult
-{
++ (BOOL)encryptPair:(NSString *)uName Pas:(NSString *)uPas Prand:(uint8_t *)prand PResult:(uint8_t *)presult {
     unsigned char *tmpNetworkName=(unsigned char *)[uName cStringUsingEncoding:NSUTF8StringEncoding];
     unsigned char *tmpPassword=(unsigned char *)[uPas cStringUsingEncoding:NSUTF8StringEncoding];
-    
     unsigned char		pNetworkName[16];
     unsigned char		pPassword[16];
-    
     memset(pNetworkName, 0, 16);
     memset(pPassword, 0, 16);
-    
     memcpy(pNetworkName, tmpNetworkName, strlen((char *)tmpNetworkName));
     memcpy(pPassword, tmpPassword, strlen((char *)tmpPassword));
-    
     unsigned char sk[16], d[16], r[16];
     int i;
-    for (i=0; i<16; i++)
-    {
+    for (i=0; i<16; i++) {
         d[i] = pNetworkName[i] ^ pPassword[i];
     }
     memcpy (sk, prand, 8);
@@ -48,70 +42,53 @@
     aes_att_encryption (sk, d, r);
     memcpy (presult, prand, 8);
     memcpy (presult+8, r, 8);
-    
-    if (!(memcmp (prand, presult, 16)))
-        return YES;
-    
+    if (!(memcmp (prand, presult, 16))) return YES;
     return NO;
 }
 
-+(BOOL)getSectionKey:(NSString *)uName Pas:(NSString *)uPas Prandm:(uint8_t *)prandm Prands:(uint8_t *)prands PResult:(uint8_t *)presult
-{
++ (BOOL)getSectionKey:(NSString *)uName Pas:(NSString *)uPas Prandm:(uint8_t *)prandm Prands:(uint8_t *)prands PResult:(uint8_t *)presult {
     unsigned char *tmpNetworkName=(unsigned char *)[uName cStringUsingEncoding:NSUTF8StringEncoding];
     unsigned char *tmpPassword=(unsigned char *)[uPas cStringUsingEncoding:NSUTF8StringEncoding];
-    
     unsigned char		pNetworkName[16];
     unsigned char		pPassword[16];
-    
     memset(pNetworkName, 0, 16);
     memset(pPassword, 0, 16);
-    
     memcpy(pNetworkName, tmpNetworkName, strlen((char *)tmpNetworkName));
     memcpy(pPassword, tmpPassword, strlen((char *)tmpPassword));
-    
     unsigned char sk[16], d[16], r[16];
     int i;
-    for (i=0; i<16; i++)
-    {
+    for (i=0; i<16; i++) {
         d[i] = pNetworkName[i] ^ pPassword[i];
     }
     memcpy (sk, prandm, 8);
     memcpy (sk + 8, prands, 8);
     aes_att_encryption (d, sk, r);
     memcpy (presult, r, 16);
-    
     return YES;
 }
 
 
-+(BOOL)encryptionPpacket:(uint8_t *)key Iv:(uint8_t *)iv Mic:(uint8_t *)mic MicLen:(int)mic_len Ps:(uint8_t *)ps Len:(int)len
-{
++ (BOOL)encryptionPpacket:(uint8_t *)key Iv:(uint8_t *)iv Mic:(uint8_t *)mic MicLen:(int)mic_len Ps:(uint8_t *)ps Len:(int)len {
     uint8_t	e[16], r[16], i;
     ///////////// calculate mic ///////////////////////
     memset (r, 0, 16);
     memcpy (r, iv, 8);
     r[8] = len;
     aes_att_encryption (key, r, r);
-        for (i=0; i<len; i++)
-    {
+    for (i=0; i<len; i++) {
         r[i & 15] ^= ps[i];
-        
-        if ((i&15) == 15 || i == len - 1)
-        {
+        if ((i&15) == 15 || i == len - 1) {
             aes_att_encryption (key, r, r);
         }
     }
-    for (i=0; i<mic_len; i++)
-    {
+    for (i=0; i<mic_len; i++) {
         mic[i] = r[i];
     }
     ///////////////// calculate enc ////////////////////////
     memset (r, 0, 16);
     memcpy (r+1, iv, 8);
-    for (i=0; i<len; i++)
-    {
-        if ((i&15) == 0)
-        {
+    for (i=0; i<len; i++) {
+        if ((i&15) == 0) {
             aes_att_encryption (key, r, e);
             r[0]++;
         }
@@ -121,43 +98,31 @@
 }
 
 
-+(BOOL)decryptionPpacket:(uint8_t *)key Iv:(uint8_t *)iv Mic:(uint8_t *)mic MicLen:(int)mic_len Ps:(uint8_t *)ps Len:(int)len
-{
++ (BOOL)decryptionPpacket:(uint8_t *)key Iv:(uint8_t *)iv Mic:(uint8_t *)mic MicLen:(int)mic_len Ps:(uint8_t *)ps Len:(int)len {
     uint8_t	e[16], r[16], i;
-    
     ///////////////// calculate enc ////////////////////////
     memset (r, 0, 16);
     memcpy (r+1, iv, 8);
-    for (i=0; i<len; i++)
-    {
-        if ((i&15) == 0)
-        {
+    for (i=0; i<len; i++) {
+        if ((i&15) == 0) {
             aes_att_encryption (key, r, e);
             r[0]++;
         }
         ps[i] ^= e[i & 15];
     }
-    
     ///////////// calculate mic ///////////////////////
     memset (r, 0, 16);
     memcpy (r, iv, 8);
     r[8] = len;
     aes_att_encryption (key, r, r);
-    
-    for (i=0; i<len; i++)
-    {
+    for (i=0; i<len; i++) {
         r[i & 15] ^= ps[i];
-        
-        if ((i&15) == 15 || i == len - 1)
-        {
+        if ((i&15) == 15 || i == len - 1) {
             aes_att_encryption (key, r, r);
         }
     }
-    
-    for (i=0; i<mic_len; i++)
-    {
-        if (mic[i] != r[i])
-        {
+    for (i=0; i<mic_len; i++) {
+        if (mic[i] != r[i]) {
             return NO;			//Failed
         }
     }
@@ -167,6 +132,7 @@
 
 +(BOOL)getNetworkInfo:(uint8_t *)pcmd Opcode:(int)opcode Str:(NSString *)str Psk:(uint8_t *)psk
 {
+    
     unsigned char *tmpNetworkName=(unsigned char *)[str cStringUsingEncoding:NSUTF8StringEncoding];
     
     unsigned char		pNetworkName[16];
